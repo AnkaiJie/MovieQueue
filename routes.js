85f8a64388b2734ce28d851aa79ceeb296bc3910
Patrick Dang-Ho <patrickdangho@gmail.com>
2015-11-28 13:38:24 -0500

    changed name from main to home. Created a temp home.ejs for testing

--- routes.js
+++ routes.js
@@ -4,14 +4,10 @@ var express = require('express');
 module.exports = function(app, passport) {
 
 	app.get('/', function(req, res) {
-<<<<<<< HEAD
-		res.render('home.ejs');
-=======
 		res.render('index.ejs');
->>>>>>> 0e871799b09df37a43e26ba9ef4bc0ad00f8e578
 	});
 
-	app.get('/homePage', isLoggedIn, function(req, res) {
+	app.get('/home', isLoggedIn, function(req, res) {
 		var name = req.user.facebook.name;
 		res.render('home.ejs', {
 			name : name
@@ -23,7 +19,7 @@ module.exports = function(app, passport) {
 	}));
 
 	app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-		successRedirect : '/homePage',
+		successRedirect : '/home',
 		failureRedirect : '/'
 	}));
 
