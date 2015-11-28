b93c64640bc8835ddb5f4df3b7899d4a7f29258d
Ankai Jie <ankaijie@gmail.com>
2015-11-28 16:21:42 -0500

    img fix

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+app.use (express.static("public"));
 app.set('view engine', 'ejs');
 
 // DB Models
